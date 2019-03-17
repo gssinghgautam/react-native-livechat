d0400dbf8ca115a663a7d9e8e2faf37f2fac0408
Gautam Kumar <gssinghgautam1@gmail.com>
2019-03-17 21:31:35 +0530

    open and close chat hooks added

--- LiveChat/LiveChat.js
+++ LiveChat/LiveChat.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
-import { View, Dimensions, StyleSheet, Image, Platform } from 'react-native';
-import PropTypes from 'prop-types';
-import ChatBubble from './ChatBubble/ChatBubble';
-import Chat from './Chat/Chat';
-import {init} from "@livechat/livechat-visitor-sdk";
+import React, { Component } from "react";
+import { View, Dimensions, StyleSheet, Image, Platform } from "react-native";
+import PropTypes from "prop-types";
+import ChatBubble from "./ChatBubble/ChatBubble";
+import Chat from "./Chat/Chat";
+import { init } from "@livechat/livechat-visitor-sdk";
 
-const chatIcon = require('../assets/chat.png');
+const chatIcon = require("../assets/chat.png");
 
-const { height, width } = Dimensions.get('window');
+const { height, width } = Dimensions.get("window");
 
 export default class LiveChat extends Component {
   constructor(props) {
@@ -15,11 +15,13 @@ export default class LiveChat extends Component {
     this.defineStyles();
     this.state = {
       isChatOn: false,
-      bubble: props.bubble ? props.bubble : (
+      bubble: props.bubble ? (
+        props.bubble
+      ) : (
         <View style={this.styles.bubbleStyle}>
           <Image source={chatIcon} style={this.styles.icon} />
         </View>
-      ),
+      )
     };
     if (!GLOBAL.visitorSDK) {
       GLOBAL.visitorSDK = init({
@@ -37,24 +39,27 @@ export default class LiveChat extends Component {
         height: width / 5,
         backgroundColor: this.props.bubbleColor,
         borderRadius: width / 10,
-        alignItems: 'center',
-        justifyContent: 'center',
+        alignItems: "center",
+        justifyContent: "center"
       },
       icon: {
-        width: width / 7, height: width / 7,
+        width: width / 7,
+        height: width / 7
       },
       container: {
-        position: 'absolute',
-      },
+        position: "absolute"
+      }
     });
   }
 
   openChat = () => {
     this.setState({ isChatOn: true });
+    this.props.onOpenChat && this.props.onOpenChat();
   };
 
   closeChat = () => {
     this.setState({ isChatOn: false });
+    this.props.onCloseChat && this.props.onCloseChat();
   };
 
   render() {
@@ -67,7 +72,11 @@ export default class LiveChat extends Component {
           bubble={this.state.bubble}
           disabled={this.props.movable}
         />
-        <Chat {...this.props} isChatOn={this.state.isChatOn} closeChat={this.closeChat} />
+        <Chat
+          {...this.props}
+          isChatOn={this.state.isChatOn}
+          closeChat={this.closeChat}
+        />
       </View>
     );
   }
@@ -84,16 +93,19 @@ LiveChat.propTypes = {
   chatTitle: PropTypes.string,
   greeting: PropTypes.string,
   noAgents: PropTypes.string,
-  onLoaded: PropTypes.func,
+  onLoaded: PropTypes.func
 };
 
 LiveChat.defaultProps = {
-  bubbleColor: '#2196F3',
+  bubbleColor: "#2196F3",
   movable: true,
   onLoaded: () => {},
-  bubbleLeft: width - (width / 5) - (width / 50),
-  bubbleTop: Platform.OS === 'ios' ? height - (width / 5) - (width / 50) : height - (width / 5) - (width / 13),
-  chatTitle: 'Chat with us!',
-  greeting: 'Welcome to our LiveChat!\nHow may We help you?',
-  noAgents: 'Our agents are not available right now.',
+  bubbleLeft: width - width / 5 - width / 50,
+  bubbleTop:
+    Platform.OS === "ios"
+      ? height - width / 5 - width / 50
+      : height - width / 5 - width / 13,
+  chatTitle: "Chat with us!",
+  greeting: "Welcome to our LiveChat!\nHow may We help you?",
+  noAgents: "Our agents are not available right now."
 };
